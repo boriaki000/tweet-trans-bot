1292ef34c64038aa52dc973b091d4ab946b2404b
boriaki000 <boriaki000@google.com>
2020-12-26 00:02:16 +0900

    mod test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,12 +1,10 @@
 import unittest
-import index
 
 
 class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("no test.")
-        self.assertEqual(200,200)
 
 
 if __name__ == '__main__':
