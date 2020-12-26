7c93bf939a0b81915ffa0e146bf6bbdefe6b85c2
boriaki000 <boriaki000@gmail.com>
2020-12-26 22:43:53 +0900

    Bug fix add retry logic

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ import os
 import requests
 import logging
 import tweepy
+from time import sleep
 from googletrans import Translator
 
 # Prepare key and token for tweepy.
@@ -27,6 +28,7 @@ target_ids = [x.strip() for x in str(os.getenv('TARGET_IDS')).split(',')]
 time_distance = int(os.environ['TIME_DISTANCE'])
 tweet_count = os.environ['TWEET_COUNT']
 basetime = datetime.datetime.now()
+retry_count = 3
 
 print('--- PARAM ---')
 print('target_ids:' + str(target_ids))
@@ -50,22 +52,26 @@ def handler(event, context):
     logger.info('END  :Post to Discord')
 
 def get_tweet(user_id):
-    try:
-        tweets = api.user_timeline(user_id, count=tweet_count, tweet_mode='extended')
-        result = []
-        for tweet in tweets:
-            distance = basetime - tweet.created_at
-            if distance.days == 0 and distance.seconds < time_distance:
-                translated = translator.translate('tranlated:' + tweet.full_text, src='en', dest='ja')
-                result.append({'user_name':tweet.user.name
-                            ,'created_at':str(tweet.created_at)
-                            ,'text':translated.text
-                            ,'url':'https://twitter.com/' + user_id + '/status/' + str(tweet.id)})
-    except Exception as e:
-        sys.stderr.write('Error occurs in get_tweet\n')
-        sys.stderr.write(str(e) + '\n')
-    else:
-        return result
+    for i in range(0, retry_count):
+        try:
+            tweets = api.user_timeline(user_id, count=tweet_count, tweet_mode='extended')
+            result = []
+            for tweet in tweets:
+                distance = basetime - tweet.created_at
+                if distance.days == 0 and distance.seconds < time_distance:
+                    translated = translator.translate('tranlated:' + tweet.full_text, src='en', dest='ja')
+                    result.append({'user_name':tweet.user.name
+                                ,'created_at':str(tweet.created_at)
+                                ,'text':translated.text
+                                ,'url':'https://twitter.com/' + user_id + '/status/' + str(tweet.id)})
+        except Exception as e:
+            sys.stderr.write('Error occurs in get_tweet\n')
+            sys.stderr.write(str(e) + '\n')
+            sleep(i * 5)
+        else:
+            return result
+    sys.stderr.write('Cannot complete get_tweet\n')
+    return False
 
 def post_to_discord(item):
     content = item['user_name'] + ' ' + item['created_at'] + '\n' + item['text'] + '\n' + item['url']
